806e33ba7ec78c38e909b0613b79d2e5f041c084
yudiancheng <lolcdkey@163.com>
2019-05-30 23:30:29 +0800

    first commit

---
+++ Rs/src/com/yu/filter/LoginFilter.java
@@ -0,0 +1,41 @@
+package com.yu.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
+        String uri = req.getRequestURI();
+        System.out.println(uri);
+        if(uri.equals("/Rs/personLogin") || uri.equals("/Rs/login.jsp")) {
+            filterChain.doFilter(request,response);
+            return;
+        } else {
+            Object user = req.getSession().getAttribute("user");
+            Object company = req.getSession().getAttribute("company");
+            if(user != null || company != null) {
+                filterChain.doFilter(request,response);
+                return;
+            } else {
+                resp.sendRedirect("/Rs/login.jsp");
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
